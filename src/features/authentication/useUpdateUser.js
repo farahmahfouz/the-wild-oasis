b92bb1a9129345fc46f66bc247e598d7d03db12e
farahmahfouz <farahmahfouz11@gmail.com>
2025-07-16 23:00:19 +0300

    feat: implement user header and updating user data and password

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,21 @@
+import toast from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateCurrentUser } from "../../services/apiAuth";
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+  const { mutate: updateUser, isPending: isUpdatig } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: () => {
+      toast.success("User updated successfully");
+      // queryClient.setQueryData("user", user);
+      queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+  return { updateUser, isUpdatig };
+}
